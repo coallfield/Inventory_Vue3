3041cebdfcd30fceaaf50e67f7be14fadcd24248
coallfield <fisunov1999@gmail.com>
2023-10-01 19:01:01 +0400

    Inventory

--- src/App.vue
+++ src/App.vue
@@ -62,26 +62,7 @@ export default {
     width: 100%;
     height: 100vh;
     background-color: black;
-    .btns {
-        color: white;
-        margin: 10px;
-        background: none;
-        border: 2px solid white;
-        outline: none;
-        border-radius: 5px;
-        width: 10%;
-        height: 5vh;   
-    }
-    .over-btns {
-        color: black;
-        margin: 10px;
-        background: white;
-        border: 2px solid white;
-        outline: none;
-        border-radius: 5px;
-        width: 10%;
-        height: 5vh;
-    }
+   
 }
 
 
